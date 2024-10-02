bd2779bf2dd36bcaefe6b29276185bf4cda45c97
rowa90ameen <159520497+rowa90ameen@users.noreply.github.com>
2024-10-02 23:04:00 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  
+      <div className="homeContainer">
+        <div className="childContainer">
+          Hey,
+          <br />
+          My Name is <span className="greenColor">Rowa</span>
+          <br />A Next.js enthusiast learner to build fast, scalable and server-rendered web with React.
+        </div>
+      </div>
+  );
+}
